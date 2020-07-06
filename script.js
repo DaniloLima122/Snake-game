d0f17400ab46361496c65f0089a443788d677f52
DaniloLima122 <limadanilo06@gmail.com>
2020-07-06 05:57:36 -0300

    definir o fim do jogo

--- script.js
+++ script.js
@@ -56,6 +56,15 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direcao == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direcao == "up") snake[0].y = 16 * box;
 
+    for (let i = 1; i < snake.length; i++) {
+
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+
+            clearInterval(jogo);
+            alert("Game over :(")
+        }
+    }
+
     criarBG();
     criarCobrinha();
     desenharComida();
