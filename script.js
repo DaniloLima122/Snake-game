31b9085a02a3edb47db058cf4d24c7ee3739ee8c
DaniloLima122 <limadanilo06@gmail.com>
2020-07-06 05:22:22 -0300

    Definir direcoes e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+
 snake[0] = {
 
     x: 8 * box,
@@ -19,13 +20,29 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event){
+
+    if(event.keyCode == 37 && direcao != "right") direcao = "left";
+    if(event.keyCode == 38 && direcao != "down") direcao = "up";
+    if(event.keyCode == 39 && direcao != "left") direcao = "right";
+    if(event.keyCode == 40 && direcao != "up") direcao = "down";
+}
+
 function criarBG() {
 
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
-}
+}    
 
 function iniciarJogo(){
+
+    if(snake[0].x > 15 * box && direcao == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direcao == "left") snake[0].x = 16 * box;
+
+    if(snake[0].y > 15 * box && direcao == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direcao == "up") snake[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
@@ -36,8 +53,8 @@ function iniciarJogo(){
     if(direcao == "right") snakeX+= box;
     if(direcao == "left") snakeX-= box;
     
-    if(direcao == "up") snakeY+= box;
-    if(direcao == "down") snakeY-= box;
+    if(direcao == "up") snakeY-= box;
+    if(direcao == "down") snakeY+= box;
 
     snake.pop();
 
